d33c13d1f1661fc02e1efe2a7efea38d3b7ed84a
Les Schaffer <les@designspring.com>
2015-02-06 18:44:27 -0500

    do proper imports implement getImmed in prep for making measurements versatile

--- tds-2024.py
+++ tds-2024.py
@@ -19,11 +19,11 @@
 
 
 
-from matplotlib.pylab import *
+from matplotlib.pylab import plot, axis, xlabel, ylabel, gca, grid, text, show
 from numpy import array
-from string import *
-from time import *
-from struct import *
+from string import split
+from time import sleep
+from struct import unpack
 from serial import Serial   # we don't need no steenkin' VISA 
 
 # how long to sleep after issuing a write
@@ -32,26 +32,26 @@ sleeptime = 0.1
 class TDS2024(Serial):
     idStr = 'TEKTRONIX,TDS 2024,0,CF:91.1CT FV:v4.12 TDS2CM:CMV:v1.04\n'
     wfmD = {}
-    _wfmD = {'BYT_NR':int,
-            'UNIT': None,
-            'BYT_NR': int,
-            'BIT_NR': int,
-            'ENCDG': None,  # ASC | BIN
-            'BN_FMT': None,
-            'BYT_OR': None,
-            'NR_PT': int,
-            'WFID': None, # "Ch3, DC coupling, 1.0E0 V/div, 1.0E-1 s/div, 2500 points, Sample mode"
-            'PT_FMT': None,
-            'XINCR': float, # 4.0E-4
-            'PT_OFF': int, # 0
-            'XZERO': float, # -1.0E-1
-            'XUNIT': None, # "s"
-            'YMULT': float, # 4.0E-2
-            'YZERO': float, # 0.0E0
-            'YOFF': float, # -4.5E1
-            'YUNIT': None #"Volts"
+    wfmFuncD = {'BYT_NR':int,
+                'UNIT': None,
+                'BYT_NR': int,
+                'BIT_NR': int,
+                'ENCDG': None,  # ASC | BIN
+                'BN_FMT': None,
+                'BYT_OR': None,
+                'NR_PT': int,
+                'WFID': None, # "Ch3, DC coupling, 1.0E0 V/div, 1.0E-1 s/div, 2500 points, Sample mode"
+                'PT_FMT': None,
+                'XINCR': float, # 4.0E-4
+                'PT_OFF': int, # 0
+                'XZERO': float, # -1.0E-1
+                'XUNIT': None, # "s"
+                'YMULT': float, # 4.0E-2
+                'YZERO': float, # 0.0E0
+                'YOFF': float, # -4.5E1
+                'YUNIT': None #"Volts"
             }
-    _wfmT = _wfmD.keys()
+    wfmT = wfmFuncD.keys()
     
     def __init__(self, port="/dev/ttyS0", debug=False):
         self._port = port
@@ -113,8 +113,8 @@ class TDS2024(Serial):
         preamble = split(tmp[8:],';')
         for resp in preamble:
             name, val = resp.split(' ',1)
-            if name in self._wfmT:
-                func = self._wfmD[name]
+            if name in self.wfmT:
+                func = self.wfmFuncD[name]
                 if func:
                     self.wfmD[name]= func(val)
                 else:
@@ -156,28 +156,28 @@ class TDS2024(Serial):
             volt_string = '%i mv / div' % (voltsdiv * 1000)
         self.voltStr = volt_string
         self.voltsdiv = voltsdiv
-        
+
+    def _getImmed(self, typ, ch):
+        self.cmd('measu:imm:typ %s;:measu:imm:sou %3s'%(typ,ch))
+        return self.query_float('measu:imm:val?')
 
     def getMeasurements(self):
         ch = self._channel        
         # get some measurements, just for fun
-        tmp = 9.9E37
-        self.cmd('measu:imm:typ PK2;:measu:imm:sou %3s'%ch)
-        tmp=self.query_float('measu:imm:val?')
+
+        tmp=self._getImmed('PK2', '%3s'%ch)
         if tmp != 9.9E37:
             peak_string = 'Pk-Pk: %.3f V' % (tmp)
         else: peak_string = ''
         self.peakStr = peak_string
         
-        self.cmd('measu:imm:typ MEAN;:measu:imm:sou %3s'%ch)
-        tmp=self.query_float('measu:imm:val?')
+        tmp=self._getImmed('MEAN', '%3s'%ch)
         if tmp != 9.9E37:
             mean_string = 'Mean: %.3f V' % (tmp)
         else: mean_string = ''
         self.meanStr = mean_string
         
-        self.cmd('measu:imm:typ PERI;:measu:imm:sou %3s'%ch)
-        tmp=self.query_float('measu:imm:val?')
+        tmp=self._getImmed('PERI', '%3s'%ch)
         if tmp >= 1:
             period_val = tmp
             period_suf = "S"
@@ -195,8 +195,7 @@ class TDS2024(Serial):
         else: period_string = ''
         self.periodStr = period_string
         
-        self.cmd('measu:imm:typ FREQ;:measu:imm:sou %3s'%ch)
-        tmp=self.query_float('measu:imm:val?')
+        tmp=self._getImmed('FREQ', '%3s'%ch)
         if tmp < 1e3:
             freq_val = tmp
             freq_suf = "Hz"
@@ -230,7 +229,7 @@ class TDS2024(Serial):
         if self._debug: print 'Acquiring %d points'%points
         tmp=self.read(self.wfmD['BYT_NR']*points+1) #newline at end???
 
-        formatstring = '%ib' % (len(tmp))
+        formatstring = '%ib' % (len(tmp))   # does this assume BYT_NR==1???
         tmplist = unpack(formatstring,tmp)
         
         yoff = self.wfmD['YOFF']
